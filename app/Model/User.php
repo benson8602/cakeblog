2669041dd2758c74273bc3a58d64c51022b86c1f
benson8602 <benson8602@yahoo.co.uk>
2014-03-12 14:27:59 +0000

    added users

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,5 +1,4 @@
 <?php
-// app/Model/User.php
 class User extends AppModel {
     public $validate = array(
         'username' => array(
@@ -22,15 +21,5 @@ class User extends AppModel {
             )
         )
     );
-    
-    public function beforeSave($options = array()) {
-    if (isset($this->data[$this->alias]['password'])) {
-        $passwordHasher = new SimplePasswordHasher();
-        $this->data[$this->alias]['password'] = $passwordHasher->hash(
-            $this->data[$this->alias]['password']
-        );
-    }
-    return true;
-}
 }
 ?>
