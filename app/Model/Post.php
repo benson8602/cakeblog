06824a7132fccc4d63a423a00152d18faadde1ed
benson8602 <benson8602@yahoo.co.uk>
2014-04-23 00:08:45 +0100

    Rename add post to New Post move the link to bottom

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -8,11 +8,22 @@ class Post extends AppModel{
 	);
 	
 	public $validate = array(
-        'title' => array(
-            'rule' => 'notEmpty'
-        ),
-        'body' => array(
-            'rule' => 'notEmpty'
+            
+            'title' => array(
+                'title_must_not_be_blank'=> array(
+                    'rule' => 'notEmpty',
+                    'message' =>'This post is missing a title!'
+                ),
+                'title_must_be_unique'=> array(
+                    'rule' => 'notEmpty',
+                    'message' =>'A post with this title already exists!'
+                )
+            ),
+            'body' => array(
+                'body_must_not_be_blank' => array(
+                    'rule' => 'notEmpty',
+                    'message' =>'This post is missing a body!'
+                    )
         )
     );
 
